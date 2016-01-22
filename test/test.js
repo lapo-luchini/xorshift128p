01520ca6f773875f586c88e2410bf2c068ca86f7
Lapo Luchini <lapo@lapo.it>
2016-01-22 18:25:46 +0000

    Refactor tests.

---
+++ test/test.js
@@ -0,0 +1,14 @@
+var XorShift128p = require('../xorshift128p');
+
+process.stdout.on('error', function (err) {
+    // stop when output pipe is closed
+    process.exit(1);
+});
+
+function hex32(i) { return ('0000000' + i.toString(16)).slice(-8); }
+function hex64(i) { return hex32(i[0]) + hex32(i[1]); }
+
+var r = new XorShift128p(1);
+
+for (var i = 0; i < 1000000; ++i)
+    process.stdout.write(hex64(r.next()) + '\n');
--- test/test2.js
+++ test/test2.js
@@ -1,16 +1,12 @@
-var XorShift128p = require('./xorshift128p');
+var XorShift128p = require('../xorshift128p');
 
 process.stdout.on('error', function (err) {
     // stop when output pipe is closed
     process.exit(1);
 });
 
-function hex32(i) {
-    return ('0000000' + i.toString(16)).slice(-8);
-}
-function hex64(i) {
-    return hex32(i[0]) + hex32(i[1]);
-}
+function hex32(i) { return ('0000000' + i.toString(16)).slice(-8); }
+function hex64(i) { return hex32(i[0]) + hex32(i[1]); }
 
 var r = new XorShift128p(1);
 for (var i = 0; i < 3; ++i)
